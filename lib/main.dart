8fe55e89c808fbbc3d26b1e9f1f45a1ec8d5f678
Asif <asifkhan858591@gmail.com>
2022-12-03 00:56:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:b_bank/screens/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Home_Screen(),
+    );
+  }
+}
---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home_Screen extends StatefulWidget {
+  Home_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Home_Screen> createState() => _Home_ScreenState();
+}
+
+class _Home_ScreenState extends State<Home_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          // blood bank text heasing container
+          Container(
+            width: MediaQuery.of(context).size.width * 1,
+            height: MediaQuery.of(context).size.height * 0.19,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25)),
+              color: Colors.redAccent,
+            ),
+            child: Center(
+              child: Text(
+                "Blood Bank",
+                style: GoogleFonts.oswald(fontSize: 59, color: Colors.white),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                // donor registraion container
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.person,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "Donor Registration",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // donors list container
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.list_alt_outlined,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "Donors List",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // item master
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.medical_information_outlined,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "Item Master",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                ///   good recived note
+
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.note_alt_outlined,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "Good Recieved Note",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //sales
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.sell_outlined,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "Sales",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // about
+
+                Container(
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.info_outline,
+                          size: 50,
+                          color: Colors.black45,
+                        ),
+                        Text(
+                          "About",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.ubuntu(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
